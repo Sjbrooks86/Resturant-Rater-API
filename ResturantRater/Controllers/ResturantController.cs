1ff8ddc319d5b6cf3544579d2a6d909f888227b9
Scott Brooks <sjbrooks86@gmail.com>
2020-02-25 13:38:10 -0500

    enabled migrations

---
+++ ResturantRater/Controllers/ResturantController.cs
@@ -0,0 +1,28 @@
+﻿using ResturantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ResturantRater.Controllers
+{   //return status codes
+    public class ResturantController : ApiController
+    {
+        private readonly ResturantDbContext _context = new ResturantDbContext();
+        // POST
+        public async Task<IHttpActionResult> PostResturant(Resturant resturant)
+        {
+            if (ModelState.IsValid && resturant != null)
+            {
+                _context.Resturants.Add(resturant);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
