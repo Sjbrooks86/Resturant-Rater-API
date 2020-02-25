a52895b41b7dad42b87383db63ebd915e6942186
Scott Brooks <sjbrooks86@gmail.com>
2020-02-25 10:44:19 -0500

    Add project files.

---
+++ ResturantRater/Models/Resturant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ResturantRater.Models
+{
+    public class Resturant
+    {
+        [Key]
+        public int Id { get; set; }            //this is the primary key, it will always be the first on top.
+        
+        [Required]
+        public string Style { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        [Range(0d, 5d)]
+        public double Rating { get; set; }
+        
+        [Required]
+        [Range(1, 5)]
+        public int DollarSigns { get; set; }
+    }
+}
---
+++ ResturantRater/Models/ResturantDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ResturantRater.Models
+{
+    public class ResturantDbContext : DbContext
+    {
+        public ResturantDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Resturant> Resturants { get; set; }
+    }
+}
